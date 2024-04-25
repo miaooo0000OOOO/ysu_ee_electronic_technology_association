3e2efb004ec80989c47483b7ede8be7c9d882df3
miaooo0000OOOO <1683618861@qq.com>
2024-04-25 12:36:26 +0800

    更新了无人机PID控制教程

--- course/else/experiment/pid_drone/drone_control_system.c
+++ course/else/experiment/pid_drone/drone_control_system.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "drone.h"
 #include "pid.h"
 #include "float.h"
 
-#define MAX_F 50
-#define g 9.8 // 重力加速度
+#define MAX_F 20
+#define g 9.8          // 重力加速度
+#define DRAG_COEFF 1 // 空气阻力系数 (随意写的数)
 
 /// @brief 在时间步长`dt`内，应用控制器`pid`控制无人机`drone`输出升力，使无人机达到目标高度`target_height`
 /// @param drone 无人机结构体
@@ -14,7 +16,7 @@
 /// @param dt 时间步长
 /// @param target_height 目标高度
 /// @return 控制量(无人机升力)
-Float update_drone(Drone *const drone, PID *const pid, Float dt, Float target_height)
+Float update_drone(Drone *const drone, PID *const pid, Float dt, Float target_height, Float t)
 {
     Float e = target_height - drone->h;
     Float F = update_pid(pid, e, dt); // 升力
@@ -27,7 +29,12 @@ Float update_drone(Drone *const drone, PID *const pid, Float dt, Float target_he
         F = -MAX_F;
     }
 
-    Float a = (F - drone->m * g) / drone->m;
+    Float F_lift = F;                 // 升力
+    Float G = -drone->m * g;          // 重力
+    Float f = -DRAG_COEFF * drone->v; // 空气阻力(不严谨的计算方法)
+    Float F_wind = 2 * sin(t);        // 风力，随意添加的扰动力
+
+    Float a = (F_lift + G + f + F_wind) / drone->m;
     drone->v += a * dt;
     drone->h += drone->v * dt;
     return F;
@@ -56,7 +63,7 @@ int main()
     while (t < end_time)
     {
         target_height = get_target_height(t); // 如果目标高度是随时间变化的函数
-        F = update_drone(&drone, &pid, dt, target_height);
+        F = update_drone(&drone, &pid, dt, target_height, t);
         if (i % ratio == 0)
         {
             if (flag)
